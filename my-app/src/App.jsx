5bc1ac4296f41b72014931294b999912d809c79e
Clarisse Estima <clarisse0estima@gmail.com>
2026-02-01 15:16:08 -0300

    feat: reorganize component structure and styles; migrate to new directory structure

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Header from './components/Header'
-import Navigation from './components/Navigation'
-import IntroSection from './components/IntroSection'
-import PostList from './components/PostList'
-import Sidebar from './components/Sidebar'
-import Footer from './components/Footer'
+import Header from './components/header/Header'
+import Navigation from './components/nav/Navigation'
+import IntroSection from './components/introSection/IntroSection'
+import PostList from './components/postList/PostList'
+import Sidebar from './components/sidebar/Sidebar'
+import Footer from './components/footer/Footer'
 
 function App() {
   const posts = [
