030abc310a5cf691b36b22189b1d8356c6a16e58
Clarisse Estima <clarisse0estima@gmail.com>
2026-02-03 12:31:17 -0300

    home page practically finished

--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -1,9 +1,13 @@
 import './Header.css'
 import logo from '../../assets/text.gif'
+import lace from '../../assets/lace.png'
 
 function Header() {
   return (
     <header className="site-header">
+      <div className="lace-container">
+        <img src={lace} alt="decorative lace" className="lace-image" />
+      </div>
       <div className="logo-container">
         <img src={logo} alt="clari est's blog logo" className="logo-image" />
       </div>
